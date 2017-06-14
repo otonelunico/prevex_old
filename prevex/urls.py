6069ebf5bb4e9f494208df9593876d1dfde23394
octavio <ocubillosj@gmail.com>
2017-06-14 16:21:59 -0400

    se sube -14 junio static

--- prevex/urls.py
+++ prevex/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     url(r'^inventory/', include('apps.inventory.urls', namespace='inventory')),
     url(r'^binnacle/', include('apps.binnacle.urls', namespace='binnacle')),
 ]
+urlpatterns += [
+    # ... the rest of your URLconf goes here ...
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
